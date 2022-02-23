1eda29ec5ac2bfe1b1eed8affe9516491ec7aa7a
Ryan Delaney <vitineth@gmail.com>
2022-02-23 09:52:10 +0000

    Adding some comments

--- ents_projector_control/controller.py
+++ ents_projector_control/controller.py
@@ -6,6 +6,8 @@ from typing import List, Dict
 from nec_pd_sdk.nec_pd_sdk import NECPD
 from nec_pd_sdk.protocol import PDError
 
+# Constants retrieved from
+# https://github.com/signageos/nec-pd-sdk/blob/17aecc4b3a98774e395cfe52ae94be6a3fbf5de6/src/constants.ts
 ON = 1
 STANDBY = 2
 SUSPEND = 3
@@ -22,6 +24,7 @@ def load_configuration():
         '/etc/ents/projectors.json',
     ]
 
+    # Try to load each location but immediately fail if the path doesn't exist, there's no point trying to read from it
     for location in locations:
         if not os.path.exists(location):
             break
@@ -29,6 +32,7 @@ def load_configuration():
                   encoding='utf8') as f:
             return json.loads(f.read())
 
+    # Loading immediately returns if it parses so if execution ends up down here the config files didn't exist
     raise FileNotFoundError("Could not find a valid configuration file at any of the available paths. "
                             "Searched: {}".format(", ".join(locations)))
 
@@ -55,6 +59,7 @@ def power(projector: str, args: List[str]):
         help()
         return
 
+    # Make sure the projector exists in the config - these values are case sensitive
     config = load_configuration()  # type: Dict
     if projector not in config['projector']:
         projectors = map(lambda x: "'{}'".format(x), config['projector'].keys())
@@ -63,6 +68,7 @@ def power(projector: str, args: List[str]):
         help()
         return
 
+    # Try and connect - this is where things will go wrong
     ip = config['projector'][projector]
     try:
         client = NECPD.from_ip_address(ip)
@@ -70,6 +76,8 @@ def power(projector: str, args: List[str]):
         print("Failed to connect to the projector due to an error: {}".format(e))
         return
 
+    # Try getting the current power state as a way to avoid sending the same state is already is (on when it's already
+    # on for example) and also as a means to test the connection. Then issue the command if the states don't match
     power_state = client.command_power_status_read()
 
     if state == 'on':
@@ -92,10 +100,13 @@ def main():
         help()
         return
 
-    [_, projector, action, *rest] = sys.argv
+    # Pull out the projector name and action as they will always be required regardless of command
+    [_, projector, action, *_] = sys.argv
     # type: str
     action = action.lower()
 
+    # Only power is implemented for the time being. When passing to a function it is best to trim the arguments so
+    # each function doesn't have to go wading through the contents
     if action == 'power':
         power(projector, sys.argv[3:])
     else:
